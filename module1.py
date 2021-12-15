111857756e00ed1195333e32e12fbdbd76cf775c
Opilane TTHK <opilane@tthk.ee>
2021-12-15 13:47:41 +0200

    Add project files.

---
+++ module1.py
@@ -0,0 +1,43 @@
+#def season(num):
+   #if num == 12 or 1 <= num <= 2:
+       #print("Talv")
+   #elif  3 <= num <= 5:
+       #print("Kevad")
+   #elif 6 <= num <= 8:
+       #print("Suvi")
+   #elif 9 <= num <= 11:
+       #print("Sügis")
+   #else:
+    #print("Tundmatu kuu")
+#n = int(input("Sisesta aastaaeg (1-12): "))
+#season(n)
+
+
+
+#def bank(a:float,years:int)->float: 
+#    for i in range(years):
+#        a*=1.1
+#    return a
+
+
+#a=bank(int(input("Euro=> ")))
+#print(a)
+
+#def is_prime(arv:int)->bool:
+#        t=0
+#        for i in range(1,arv+1):
+#            if arv%i==0:  t+=1
+#        if t==2:     
+#            t=True
+#        else:
+#            t=False
+#        return t
+import datetime
+day=int(input("Введи День: "))
+month=int(input("Введи Месяц: "))
+year=int(input("Введи Год: "))
+print(day,month,year)
+
+
+
+
